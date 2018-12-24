3630be383f7422af769536a015afda1d7c8e5166
Craig Pounds <nweaver@windstream.net>
2018-12-24 13:17:39 -0600

    Removed App*.js files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Board from './components/board';
 import './index.css';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Board />,
@@ -12,5 +12,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
 
